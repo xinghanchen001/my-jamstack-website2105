927e8396740f856b1f1d4545e7ff8cd01d070797
Hanchen Xing <hanchen.xing@outlook.com>
2024-07-24 00:29:28 +0200

    Add password protection to About page

---
+++ admin/eslint.config.js
@@ -0,0 +1,22 @@
+import prettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+  plugins: [prettier],
+};
