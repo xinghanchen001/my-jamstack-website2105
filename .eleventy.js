d8edda94b433b422ba5f414a9dd08d25b43b3434
xinghanchen001 <113475988+xinghanchen001@users.noreply.github.com>
2024-07-25 22:58:28 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,28 @@
 const markdownIt = require("markdown-it");
+const md = new markdownIt({
+  html: true,
+});
 
-    // Add within your config module
-    const md = new markdownIt({
-    html: true,
-    });
-            
+const createCollectionsAndFilters = require('./_utils/index.js');
 
-      const createCollectionsAndFilters = require('./_utils/index.js');
-      
-      module.exports = function(eleventyConfig) {
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter("markdown", (content) => {
+    if (typeof content === "string") {
+      return md.render(content);
+    }
+    return content;
+  });
 
-        eleventyConfig.addFilter("markdown", (content) => {
-            if (typeof content == "string") {
-                return md.render(content);
-              }
-              return content;
-          });
-               
-        eleventyConfig.addPassthroughCopy({"theme/assets": "assets"});
+  eleventyConfig.addPassthroughCopy({"theme/assets": "assets"});
+  eleventyConfig.addPassthroughCopy("admin");
 
-        eleventyConfig.addPassthroughCopy("admin");
-        
-        createCollectionsAndFilters(eleventyConfig);
-        
-        return {
-          dir: {
-            input: "cms",
-            includes: "../theme",
-            output: "public"
-          }
-        };
-      };
+  createCollectionsAndFilters(eleventyConfig);
+
+  return {
+    dir: {
+      input: "cms",
+      includes: "../theme",
+      output: "public",
+    },
+  };
+};
